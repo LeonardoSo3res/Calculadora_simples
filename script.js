c0bf0971c437d0511129c04bdf44203d01c43bd2
Leonardo Soares <leonardo.soaressantos@hotmail.com>
2026-03-05 22:39:09 -0300

    Remove commented instructions from script.js
    Removed commented-out instructions for a calculator program.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/*Crie um programa em JavaScript que peça ao usuário para inserir dois números e uma operação matemática
-(+, -, *, /, %, **). O programa deve processar a operação escolhida e exibir o resultado. 
-A manipulação para a entrada dos dados e a apresentação do resultado deve ocorre na página HTML.*/
-
 const buttons = document.querySelectorAll('.num');
 const display = document.querySelector('.value');
 
